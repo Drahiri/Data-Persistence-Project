b6a0699b2676ec58e67a7558553476b605ca5aa6
Drahiri <kodzidig@o2.pl>
2024-11-29 08:47:18 +0100

    Implement menu buttons
    Start Button starts game.
    Quit Button exits game.

---
+++ Assets/Scripts/UIMenuHandler.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenuHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
